d14b221824e537764ff1a7f8c44316130f224b65
Tobias Deutsch <deutsch.tobias@googlemail.com>
2017-11-30 13:02:55 +0100

    Der erste Button hat jetzt eine Funktion in JS!

---
+++ script.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("DetailText").innerHTML = "Die Darstellung des Verbrauches erfolt in zwei verschiedenen Darstellungsarten, zum einen als Liste, zum andere als Diagramm. Die Liste ermöglicht dem Kunden das Ablesen der gefahrenen Strecke, die vorhandene Energie und der Verbrauch pro kilometer. In dieser Anzeige ist es ebenfalls möglich mehrere Fahrten abzulesen, über den kompletten Lebenszyklus des Tesla Autos. Das Diagramm verbildlicht die selben Daten für eine schöne übersicht in einem Kurvendiagramm.";
+}
